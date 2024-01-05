70186dbb927b908336c32910f969fef990909e87
VadimZo <vadim007zc8@gmail.com>
2024-01-05 19:50:41 +0300

    fix path

--- scripts/base.js
+++ scripts/base.js
@@ -3,6 +3,6 @@ document.currentScript.insertAdjacentHTML(
   `
 <link rel="stylesheet" href="styles/base.css" type="text/css"/>
 <link rel="shortcut icon" href="favicon.png" type="image/png" />
-<link rel="preload" href="/fonts/Caveat/Caveat-Bold.ttf" as="font" crossorigin />
+<link rel="preload" href="../fonts/Caveat/Caveat-Bold.ttf" as="font" crossorigin />
 `
 );
